e2c3b0614e015f7e5d61e5a679ec6044e37a4962
Alvaro <alvaroballarini2010@gmail.com>
2026-07-30 09:11:14 -0300

    Feature: al crear un usuario se crea con categorias por default

--- src/features/user/data/create.ts
+++ src/features/user/data/create.ts
@@ -1,5 +1,4 @@
-'use server'
-
+"use server";
 
 import prisma from "@/lib/prisma";
 import { CreateUserInput } from "../types/schemaUser";
@@ -12,6 +11,34 @@ async function createUser({ email, name, passwordHash }: CreateUserInput) {
       email,
       name,
       password: hash,
+      categories: {
+        create: [
+          {
+            color: "#155512",
+            icon: "ArrowDown",
+            name: "Vivienda",
+            type: "EXPENSE",
+          },
+          {
+            color: "#cb0909",
+            icon: "ArrowDown",
+            name: "Vivienda",
+            type: "EXPENSE",
+          },
+          {
+            color: "#553112",
+            icon: "ArrowDown",
+            name: "Vivienda",
+            type: "EXPENSE",
+          },
+          {
+            color: "#ff6a00",
+            icon: "ArrowDown",
+            name: "Vivienda",
+            type: "EXPENSE",
+          },
+        ],
+      },
     },
   });
 }
