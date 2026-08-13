67df584c3bcaf16f869dd1da55fe98ff37d9e9a4
Alvaro <alvaroballarini2010@gmail.com>
2026-08-13 15:46:46 -0300

    Feature: se agrego cache a getUser

--- src/features/auth/service/getCurrentUser.ts
+++ src/features/auth/service/getCurrentUser.ts
@@ -1,51 +1,39 @@
 import { cookies } from "next/headers";
 import getSessionService from "./getSessionService";
 import { AuthResult } from "../types/authType";
+import { cache } from "react";
 
-async function getCurrentUser(): Promise<AuthResult> {
-  const totalStart = performance.now();
-  let start = performance.now();
+const getCurrentUser = cache(async (): Promise<AuthResult> => {
+  {
+    const cookieStore = await cookies();
 
-  const cookieStore = await cookies();
+    const token = cookieStore.get("session")?.value;
+    if (!token) {
+      return {
+        authenticated: false,
+        reason: "NO_COOKIE",
+      };
+    }
 
-  console.log(
-    `[Dashboard] cookieStore: ${(performance.now() - start).toFixed(2)}ms`,
-  );
-  start = performance.now();
-  const token = cookieStore.get("session")?.value;
-  if (!token) {
+    const session = await getSessionService({ token: token });
+
+    if (!session) {
+      return {
+        authenticated: false,
+        reason: "SESSION_NOT_FOUND",
+      };
+    }
+
+    if (session.expiresAt < new Date()) {
+      return {
+        authenticated: false,
+        reason: "SESSION_EXPIRED",
+      };
+    }
     return {
-      authenticated: false,
-      reason: "NO_COOKIE",
+      authenticated: true,
+      session: session,
     };
   }
-
-  const session = await getSessionService({ token: token });
-  console.log(
-    `[Dashboard] getSessionService: ${(performance.now() - start).toFixed(2)}ms`,
-  );
-  start = performance.now();
-
-  if (!session) {
-    return {
-      authenticated: false,
-      reason: "SESSION_NOT_FOUND",
-    };
-  }
-
-  if (session.expiresAt < new Date()) {
-    return {
-      authenticated: false,
-      reason: "SESSION_EXPIRED",
-    };
-  }
-  console.log(
-    `[Dashboard] TOTAL: ${(performance.now() - totalStart).toFixed(2)}ms`,
-  );
-  return {
-    authenticated: true,
-    session: session,
-  };
-}
-
+});
 export default getCurrentUser;
